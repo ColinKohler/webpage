8d10d9e6e602dd752deefff0ea411e491bc65bb0
ColinKohler <ColinKohler@users.noreply.github.com>
2022-07-27 18:20:51 +0000

    jekyll build from Action ea0d346d7d5a931a7e318be38a75bc86986cd5d3

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,31 @@
+var store = [{
+        "title": "Deictic Image Maps: An Abstraction For Learning Pose Invariant Manipulation Policies",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/webpage/research/deictic_image_maps/",
+        "teaser": null
+      },{
+        "title": "Towards Assistive Robotic Pick and Place in Open World Environments",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/webpage/research/robotic_scooter/",
+        "teaser": null
+      },{
+        "title": "Policy learning in SE(3) action spaces",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/webpage/research/se2_policy_learning/",
+        "teaser": null
+      },{
+        "title": "BulletArm: An Open-Source Robotic Manipulation Benchmark and Learning Framework",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/webpage/research/BulletArm/",
+        "teaser": null
+      },{
+        "title": "Visual Foresight with a Local Dynamics Model",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/webpage/research/visual_foresight/",
+        "teaser": null
+      }]
